2b341f79d01814d4d801280ebfddb7fe5752b71c
Arsen <kalpazang@gmail.com>
2012-09-30 19:34:54 +0300

    changed icon, added menu

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -3,6 +3,9 @@ package com.example;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,7 +26,7 @@ public class MyActivity extends Activity {
         countdown();
     }
 
-    public void countdown(){
+    public void countdown() {
         final TextView textTime = (TextView) findViewById(R.id.textTime);
         final Calendar finalDate = Calendar.getInstance();
         finalDate.set(2012, 11, 21, 0, 0);
@@ -33,31 +36,64 @@ public class MyActivity extends Activity {
                 Calendar startDate = Calendar.getInstance();
                 startDate.setTime(new Date());
                 long timeLeft = finalDate.getTimeInMillis() - startDate.getTimeInMillis();
-                long daysLeft = timeLeft/(1000*60*60*24);
-                long hourLeft = (timeLeft/(1000*60*60))-(24*daysLeft);
-                long minutesLeft = (timeLeft/(1000*60))-(24*60*daysLeft)-(60*hourLeft);
-                long secondsLeft = (timeLeft/(1000))-(24*60*60*daysLeft)-(60*60*hourLeft)-(60*minutesLeft);
-                long millisecLeft = timeLeft-(24*60*60*1000*daysLeft)-(60*60*1000*hourLeft)-(60*1000*minutesLeft)-(1000*secondsLeft);
-                String left = "You will die in\n ...\n wait... wait...\n"+ daysLeft+" days\n"+ hourLeft+" hours\n"+
-                        minutesLeft+" min\n"+secondsLeft+" sec\n" + millisecLeft + "\nmilliseconds";
+                long daysLeft = timeLeft / (1000 * 60 * 60 * 24);
+                long hourLeft = (timeLeft / (1000 * 60 * 60)) - (24 * daysLeft);
+                long minutesLeft = (timeLeft / (1000 * 60)) - (24 * 60 * daysLeft) - (60 * hourLeft);
+                long secondsLeft = (timeLeft / (1000)) - (24 * 60 * 60 * daysLeft) - (60 * 60 * hourLeft) - (60 * minutesLeft);
+                long millisecLeft = timeLeft - (24 * 60 * 60 * 1000 * daysLeft) - (60 * 60 * 1000 * hourLeft) - (60 * 1000 * minutesLeft) - (1000 * secondsLeft);
+                String left = "You will die in...\n wait... wait...\n" + daysLeft + " days\n" + hourLeft + " hours\n" +
+                        minutesLeft + " min\n" + secondsLeft + " sec\n" + millisecLeft + "\nmilliseconds";
                 textTime.setText(left);
             }
+
             @Override
             public void onFinish() {
                 textTime.setText("BOOM!");
             }
         }.start();
     }
-    public void buttonCreate(){
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                showMessage("All the same, this does not stop. I'll wait for you");
+                System.exit(0);
+                return true;
+            case R.id.help:
+                showMessage("Who will help you?");
+
+                showMessage("No one!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void buttonCreate() {
         final Toast toast = Toast.makeText(getApplicationContext(),
                 "is not available for you, sorry :(", Toast.LENGTH_SHORT);
-        toast.setGravity(0,0,0);
+        toast.setGravity(0, 0, 0);
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new Button.OnClickListener(){
+        button.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View view) {
                 toast.show();
             }
         });
+    }
+
+    public void showMessage(String string) {
+        final Toast toast = Toast.makeText(getApplicationContext(),
+                string, Toast.LENGTH_SHORT);
+        toast.setGravity(0, 0, 0);
+        toast.show();
     }
 }
 
