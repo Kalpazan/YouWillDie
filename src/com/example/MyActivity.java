25230adb3ad6b152d9d631789804eb0ede483c6d
Art <groovy.art@gmail.com>
2012-09-30 22:00:26 +0300

    screen rotation fixed. calculations changed

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,5 +1,12 @@
 package com.example;
 
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+import static android.widget.Toast.LENGTH_SHORT;
+import static java.util.Calendar.DECEMBER;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -7,21 +14,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Calendar;
-import java.util.Date;
-
 public class MyActivity extends Activity {
-    /**
+    private static final int MINUTE = 1000 * 60;
+	private static final int HOUR = MINUTE * 60;
+
+	/**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
         buttonCreate();
         countdown();
     }
@@ -29,21 +39,25 @@ public class MyActivity extends Activity {
     public void countdown() {
         final TextView textTime = (TextView) findViewById(R.id.textTime);
         final Calendar finalDate = Calendar.getInstance();
-        finalDate.set(2012, 11, 21, 0, 0);
+        finalDate.set(2012, DECEMBER, 21, 0, 0);
         new CountDownTimer(finalDate.getTimeInMillis(), 1) {
             @Override
-            public void onTick(long l) {
-                Calendar startDate = Calendar.getInstance();
-                startDate.setTime(new Date());
-                long timeLeft = finalDate.getTimeInMillis() - startDate.getTimeInMillis();
-                long daysLeft = timeLeft / (1000 * 60 * 60 * 24);
-                long hourLeft = (timeLeft / (1000 * 60 * 60)) - (24 * daysLeft);
-                long minutesLeft = (timeLeft / (1000 * 60)) - (24 * 60 * daysLeft) - (60 * hourLeft);
-                long secondsLeft = (timeLeft / (1000)) - (24 * 60 * 60 * daysLeft) - (60 * 60 * hourLeft) - (60 * minutesLeft);
-                long millisecLeft = timeLeft - (24 * 60 * 60 * 1000 * daysLeft) - (60 * 60 * 1000 * hourLeft) - (60 * 1000 * minutesLeft) - (1000 * secondsLeft);
-                String left = "You will die in...\n wait... wait...\n" + daysLeft + " days\n" + hourLeft + " hours\n" +
-                        minutesLeft + " min\n" + secondsLeft + " sec\n" + millisecLeft + "\nmilliseconds";
-                textTime.setText(left);
+            public void onTick(long timeLeft) {
+            	timeLeft = finalDate.getTimeInMillis() - System.currentTimeMillis();
+            	
+            	long daysLeft = timeLeft / (HOUR * 24);
+                long hourLeft = (timeLeft / HOUR) - (24 * daysLeft);
+                long minutesLeft = (timeLeft % HOUR) / MINUTE;
+                long secondsLeft = (timeLeft % MINUTE) / 1000;
+                long millisecLeft = timeLeft % 1000;
+                
+                String left = "You will die in...\n wait... wait...\n " +
+                		"%s days\n " +
+                		"%s hours\n " +
+                		"%s min\n " +
+                		"%s sec\n " +
+                		"%s \nmilliseconds";
+                textTime.setText(String.format(left, daysLeft, hourLeft, minutesLeft, secondsLeft, millisecLeft));
             }
 
             @Override
@@ -90,8 +104,7 @@ public class MyActivity extends Activity {
     }
 
     public void showMessage(String string) {
-        final Toast toast = Toast.makeText(getApplicationContext(),
-                string, Toast.LENGTH_SHORT);
+        final Toast toast = Toast.makeText(getApplicationContext(), string, LENGTH_SHORT);
         toast.setGravity(0, 0, 0);
         toast.show();
     }
