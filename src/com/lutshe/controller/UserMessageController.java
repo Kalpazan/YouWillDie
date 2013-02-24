e674cae3e913711911afafdfa68b3ada7c3e00ff
Art <groovy.art@gmail.com>
2013-02-24 17:36:56 +0200

    UserMessageControler changed

--- src/com/lutshe/controller/UserMessageController.java
+++ src/com/lutshe/controller/UserMessageController.java
@@ -15,70 +15,57 @@ import com.lutshe.MainActivity;
 import com.lutshe.R;
 
 public class UserMessageController {
-	private static final int FADE_DELAY = 1500;
-	private View msgView;
-	private TextView textView;
-	private MainActivity activity;
-	private boolean isViewVisible;
+	private static final int FADE_DELAY = 1350;
+	
+	private final LinearLayout topLayout;
+	private final LayoutInflater layoutInflater;
 	
 	public UserMessageController(MainActivity activity) {
-		this.activity = activity;
+		topLayout = (LinearLayout) activity.findViewById(R.id.top_layout);
+		layoutInflater = (LayoutInflater) activity.getSystemService(LAYOUT_INFLATER_SERVICE);
 	}
 	
-	public synchronized void showMessage(String message) {
-		if (!isViewVisible) {
-            LinearLayout topLayout = (LinearLayout) activity.findViewById(R.id.top_layout);
-            topLayout.addView(getView(message));
-            isViewVisible = true;
-            msgView.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    hideMessageView();
-                }
-            }, 1500);
-        }
+	public void showMessage(String message) {
+		final View msgView = createMessageView(message);
+		topLayout.addView(msgView);
+		msgView.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				hideMessageView(msgView);
+			}
+		}, 2000);
 	}
 	
-	public synchronized void hideMessageView() {
-		if (isViewVisible) {
-            Animation fadeout = new AlphaAnimation(1.f, 0.f);
-            fadeout.setDuration(FADE_DELAY);
-            msgView.startAnimation(fadeout);
-            msgView.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    ((LinearLayout) activity.findViewById(R.id.top_layout)).removeView(getView(null));
-                    isViewVisible = false;
-                }
-            }, FADE_DELAY);
-        }
+	private void hideMessageView(final View msgView) {
+		Animation fadeout = new AlphaAnimation(1.f, 0.f);
+		
+		fadeout.setDuration(FADE_DELAY);
+		fadeout.setAnimationListener(new Animation.AnimationListener() {
+			@Override public void onAnimationStart(Animation animation) {}
+			@Override public void onAnimationRepeat(Animation animation) {}
+			
+			@Override public void onAnimationEnd(Animation animation) {
+				topLayout.removeView(msgView);
+			}
+		});
+		
+		msgView.startAnimation(fadeout);
 	}
 
-	public void viewClicked() {
-		hideMessageView();
-	}
-	
-	private View getView(String msgText) {
-		if (msgView == null) {
-			msgView = ((LayoutInflater) activity.getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.top_message, null);	
-			msgView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-			msgView.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					viewClicked();
-				}
-			});
-			textView = (TextView) msgView.findViewById(R.id.user_message_text);
-		}
+	private View createMessageView(String msgText) {
+		View msgView = layoutInflater.inflate(R.layout.top_message, null);	
+		msgView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		msgView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				hideMessageView(v);
+			}
+		});
 		
-		if (msgText != null) {
-			textView.setText(msgText);
-		}
+		TextView textView = (TextView) msgView.findViewById(R.id.user_message_text);
+		textView.setText(msgText);
 		
 		return msgView;
 	}
 
-	public synchronized boolean isVisible() {
-		return isViewVisible;
-	}
 }
