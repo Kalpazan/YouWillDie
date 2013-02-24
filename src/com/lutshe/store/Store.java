57b8cadcc0affcc1801ab7378a689ee145f52f4f
artur-tamazian <groovy.art@gmail.com>
2013-02-24 20:48:00 +0200

    old users are happy now

--- src/com/lutshe/store/Store.java
+++ src/com/lutshe/store/Store.java
@@ -8,11 +8,13 @@ import android.util.SparseArray;
 
 public class Store {
 
+	private static final String WAS_LAUNCHED_PROPERTY = "wasLaunched";
 	private static final String APOCALYPSE_TIME = "apocalyposeTime";
 	private static final String POINTS = "points";
 	private static final String TIME_PREFIX = "time_of_";
 	private static final String LAST_NOTIFICATION_NUM = "lastNotificationNum";
-	private static final String STORE_NAME = "huj";
+	private static final String STORE_NAME = "huj_vozrozhdennij";
+	private static final String OLD_STORE_NAME = "huj";
 	
 	private SparseArray<Long> notificationTimesCache = new SparseArray<Long>();
 	
@@ -23,6 +25,16 @@ public class Store {
         registerCountdownTime();
 	}
 
+	public boolean containsOldVersionData() {
+		SharedPreferences settings = context.getSharedPreferences(OLD_STORE_NAME, 0);
+		return settings.contains(WAS_LAUNCHED_PROPERTY);
+	}
+	
+	public void cleanupOldVersionData() {
+		SharedPreferences settings = context.getSharedPreferences(OLD_STORE_NAME, 0);
+		settings.edit().clear().commit();
+	}
+	
 	public void saveNotificationTime(int notificationNumber, long time) {
 	    SharedPreferences settings = context.getSharedPreferences(STORE_NAME, 0);
 	    SharedPreferences.Editor editor = settings.edit();
@@ -72,11 +84,11 @@ public class Store {
 	}
 
 	public void registerFirstLaunch() {
-		context.getSharedPreferences(STORE_NAME, 0).edit().putBoolean("wasLaunched", true).commit();
+		context.getSharedPreferences(STORE_NAME, 0).edit().putBoolean(WAS_LAUNCHED_PROPERTY, true).commit();
 	}
 
 	public boolean wasLaunchedBefore() {
-		return context.getSharedPreferences(STORE_NAME, 0).getBoolean("wasLaunched", false);
+		return context.getSharedPreferences(STORE_NAME, 0).getBoolean(WAS_LAUNCHED_PROPERTY, false);
 	}
 
     public void registerPointsAddingOnCreate() {
